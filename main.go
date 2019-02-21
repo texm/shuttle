b5d0ddffb47077fc48816e6cc170976f125901ab
Tex Moran <tmoran@freelancer.com>
2019-02-22 09:10:18 +1100

    Initial setup

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
+	//"github.com/RocketChat/Rocket.Chat.Go.SDK/rest"
+	//"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
+)
+
+func main() {
+	// 
+}
