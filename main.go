f9e7656f93567e41db505986bbb4d4b77016591b
billz <bill.zsj95@gmail.com>
2019-02-22 14:22:53 +1100

    auth

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
-	//"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
-	//"github.com/RocketChat/Rocket.Chat.Go.SDK/rest"
-	//"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
-
-	//"github.com/marcusolsson/tui-go"
+	// "encoding/json"
+	// "fmt"
+	// "github.com/RocketChat/Rocket.Chat.Go.SDK/models"
+	"fmt"
+	"github.com/Billz95/Rocket.Chat.Go.SDK/rest"
+	"net/url"
 )
 
 func main() {
-	// 
+	//
+	client := rest.NewClient(&url.URL{Host: "chat.tools.flnltd.com:80"}, false)
+	AppLogin(client)
+	// credential := models.UserCredentials{}
+
+	// se, _ := json.Marshal(models.UserCredentials{ID: "abc", Token: "kkk", Email: "mail"})
+	// fmt.Println(se)
+	// fmt.Println(string(se))
+	// dat := models.UserCredentials{}
+	// json.Unmarshal(se, &dat)
+	// fmt.Println(dat)
+
+	// client.LoginViaGoogle(&credential)
+	// fmt.Println(credential)
+	res, err := client.GetPublicChannels()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
 }
